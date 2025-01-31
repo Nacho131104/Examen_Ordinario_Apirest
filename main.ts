023a90f8dff59cd48b8f523fac9c6f61fe9166eb
Nacho131104 <nacholopezamat@gmail.com>
2025-01-31 12:22:40 +0100

    update

---
+++ main.ts
@@ -0,0 +1,41 @@
+/*
+import { MongoClient } from "mongodb";
+
+const MONGO_URL = Deno.env.get("MONGO_URL");
+
+if (!MONGO_URL) {
+  throw new Error("Please provide a MONGO_URL");
+}
+
+const mongoClient = new MongoClient(MONGO_URL);
+await mongoClient.connect();
+
+console.info("Connected to MongoDB");
+
+
+const mongoDB = mongoClient.db("");
+*/
+
+
+const handler = async (req: Request): Promise<Response> => {
+  const method = req.method;
+  const url = new URL(req.url);
+  const path = url.pathname;
+
+  if (method === "GET") {
+    
+  } 
+  else if (method === "POST") {
+    
+  } 
+  else if (method === "PUT") {
+    
+  } 
+  else if (method === "DELETE") {
+    
+  }
+
+  return new Response("Endpoint not found", { status: 404 });
+};
+
+Deno.serve({ port: 3000 }, handler);
